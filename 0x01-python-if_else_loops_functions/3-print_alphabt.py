96977697e975df3a907fd963ba9f012f8d29afba
Great man <godswillidisi@gmail.com>
2022-11-30 03:03:22 -0800

    alphabet soup

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for letter in range(ord(a), ord(z)+1):
+    if letter !='q'and letter!= 'e'
+    print({:c}.format(letter), end="")
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(1,100):
+    print("{}, ".format(i))
+    i += 1
