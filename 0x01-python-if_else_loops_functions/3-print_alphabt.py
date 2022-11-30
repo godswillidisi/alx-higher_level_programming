f4ddde310f0461f2b77cc8c2688e713133c7aab8
Great man <godswillidisi@gmail.com>
2022-11-30 03:20:33 -0800

    alphabet soup

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(ord(a), ord(z)+1):
-    if letter !='q'and letter!= 'e'
-    print({:c}.format(letter), end="")
+for letter in range(ord('a'), ord('z')+1):
+    if letter != 101 and letter != 113:
+        print("{:c}".format(letter), end="")
